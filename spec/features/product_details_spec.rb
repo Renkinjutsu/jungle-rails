b8ba0c033a641ea4257cfbd2fa8cd36d7ab44c45
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-18 17:45:57 -0400

    added test for navigation to products show page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "clicking on the details button should navigate to show page" do
+
+    visit root_path
+    save_screenshot
+    link = click_on @category.products.first.name
+    expect(visit(link)["status"]).to eq("success")
+
+  end
+
+end
